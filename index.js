f6ab9ccbf7ce6e86f1aa01da83982d04b94242e5
giokakee <75224359+giokakee@users.noreply.github.com>
2021-04-05 21:33:39 +0400

    added to mongoDB

--- index.js
+++ index.js
@@ -2,87 +2,76 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const axios = require('axios')
+const Person = require('./models/mongo')
+const mongoose = require('mongoose')
+
+
+
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-let phonebook = [
-    {   
-        id:1,
-        name: "kakee",
-        number: 300499588,
-        date: new Date()
-    },
-    {
-        id:2,
-        name: "second",
-        number: 300499588,
-        date: new Date()
-    },
-    {
-        id:3,
-        name: "third",
-        number: 300499588,
-        date: new Date()
-    },
-    {
-        id:4,
-        name: "fourth",
-        number: 324324324,
-        date: new Date()
-    }
-]
+
 
 //GET
 app.get('/', (req,res) => {
     res.send('<h1>Hello world</h1>')
 })
-app.get('/api/phonebook', (req,res) => {
-       
-   res.json(phonebook)
+app.get('/api/phonebook', async (req,res) => {
+ try{
+     const post = await Person.find({})
+     res.json(post)
+ } catch(err){
+     res.json({message: err})
+ }
 })
 app.get('/api/phonebook/:id', (req,res) => {
-    let id = req.params.id
-    let person = phonebook.find(person => person.id == id)
-     if(person){
-        res.json(person)
-     }else{
-         res.status(404).end('this person does not exists')
-     }
+    // let id = req.params.id
+    // let person = phonebook.find(person => person.id == id)
+    //  if(person){
+    //     res.json(person)
+    //  }else{
+    //      res.status(404).end('this person does not exists')
+    //  }
 })
 
 //POST
-const generatedId = () => {
-    let maxId = phonebook.length > 0
-    ?Math.max(...phonebook.map(mp => mp.id)) 
-       : 0
-    return maxId + 1
-}
-app.post('/api/phonebook', (req,res) => {
-    let body = req.body
-    
-    if(!body.name || !body.number){
-        res.status(404).end('content is missing')
-    }else if(phonebook.find(p => p.name == body.name)){
-        res.status(404).end('this person already exists')
-    }else{
-        const newPerson = {
-            id: generatedId(),
-            name: body.name,
-            number: body.number,
-            date: new Date()
-        }   
-       phonebook = phonebook.concat(newPerson)
-       res.send(phonebook)
-    }
-       
-})
+// const generatedId = () => {
+//     let maxId = phonebook.length > 0
+//     ?Math.max(...phonebook.map(mp => mp.id)) 
+//        : 0
+//     return maxId + 1
+// }
+app.post('/api/phonebook', async  (req,res) => {
+      let body = req.body 
+      try{
+          const person = await new Person ({
+              name: body.name,
+              number: body.number,
+              date: new Date()
+            }) 
+            await  person.save()
+            console.log('zimbabue')
+            console.log(`person ${body.name} saved`)
+            const post = await Person.find({})
+            res.json(post)
+        } catch(err){
+            res.json({message: err})
+        }    
+});
 
-app.delete('/api/phonebook/:id', (req, res) => {
-    let id = Number(req.params.id)
-
-    phonebook = phonebook.filter(filt => filt.id!==id)
-    res.send(phonebook)
-})
+app.delete('/api/phonebook/:id', async (req, res, next) => {
+     let id = req.params.id
+Person.findByIdAndRemove(id)
+    .then(removedPerson => {
+        Person.find({})
+            .then(persons => {
+                console.log(`${removedPerson.name} removed`)
+                res.json(persons)
+            }).catch(err => next(err))
+    }).catch(err => {
+        next(err)
+    })
+});
 
 
 
@@ -93,6 +82,19 @@ const unknownEndpoint = (request, response) => {
   }
   
   app.use(unknownEndpoint)
+
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  app.use(errorHandler)
 
 
   const PORT = process.env.PORT || 3001
---
+++ models/mongo.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+
+
+const url = process.env.url
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+
+const PersonSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minlength: 3,
+      required: true
+    },
+    number: String,
+    date: Date
+})
+
+PersonSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
+module.exports = mongoose.model('Person', PersonSchema)
